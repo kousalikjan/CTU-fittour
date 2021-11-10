edbfa2dd51e82efc6d4936056de3f696dc4d17cd
Jan Kousalík <kousaja1@cvut.cz>
2021-11-10 13:15:36 +0100

    [Add] RestServerClass

---
+++ src/main/java/cz/fit/cvut/tjv/fittour/RestServer.java
@@ -0,0 +1,13 @@
+package cz.fit.cvut.tjv.fittour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServer
+{
+    public static void main(String ...args)
+    {
+        SpringApplication.run(RestServer.class, args);
+    }
+}
